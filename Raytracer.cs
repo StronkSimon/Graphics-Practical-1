3fb469740bc2c6073252390ae34968b4b73eedf4
Simon de Jong <simondj2005@gmail.com>
2024-05-27 13:00:05 +0200

    fix

--- Raytracer.cs
+++ Raytracer.cs
@@ -53,7 +53,7 @@ namespace RayTracer
             scene.lightSources.Add(new Light((4f, 1f, 0f), (1f, 1f, 1f)));
             scene.lightSources.Add(new Light((-2f, 2f, -1f), (1f, 1f, 1f)));
             scene.lightSources.Add(new Light((0f, 4f, 4f), (1f, 1f, 1f)));
-            for (int i = 0; i < width/2; i++)
+            for (int i = 0; i < width; i++)
             {
                 for (int j = 0; j < height; j++)
                 {
